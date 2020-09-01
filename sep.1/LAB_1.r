42c3c2fa27bb5890cb88ec2bb587e8517b81ef84
nathenbyford <54156788+nathenbyford@users.noreply.github.com>
2020-09-01 13:29:24 -0500

    Completed Lab 1

--- sep.1/LAB_1.r
+++ sep.1/LAB_1.r
@@ -20,4 +20,33 @@ faithful$waiting %>% summary()
 ggplot(faithful, aes(x = eruptions, y = waiting)) +
     geom_point()
 
-cars
+# New data set from Web
+bodyfat <- read.table("http://www.jpstats.org/Regression/data/BodyFat.txt",
+    header = TRUE)
+
+bodyfat$bfat %>% summary()
+
+ggplot(data = bodyfat, aes(x = tri, y = bfat)) +
+    geom_point()
+
+
+## Set wd to data folder
+setwd("STA3386")
+setwd("data")
+
+data <- read.table("housingprices.csv", header = TRUE, sep = ",")
+
+data %>% summary()
+
+ggplot(data, aes(x = age, y = salePrice)) +
+    geom_point()
+
+# GPA exercise
+rm(data)
+
+data <- read.table("gpa.txt", header = TRUE)
+
+data %>% summary()
+
+ggplot(data, aes(x = act, y = gpa)) +
+  geom_point(col = "steel blue")
