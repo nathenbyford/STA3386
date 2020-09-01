470004b3ef1fabf47e2507a085fbeff4aa8e3dba
nathenbyford <54156788+nathenbyford@users.noreply.github.com>
2020-09-01 12:33:43 -0500

    Edited

--- Sep_1..r
+++ Sep_1..r
@@ -2,8 +2,8 @@ rm(list = ls())
 
 library(tidyverse)
 
-x <- c( 1, 2, 2.75, 4, 6, 7, 8, 10)
-y <- c( 2, 1.4, 1.6, 1.25, 1, 0.5, 0.5, 0.4)
+x <- c(1, 2, 2.75, 4, 6, 7, 8, 10)
+y <- c(2, 1.4, 1.6, 1.25, 1, 0.5, 0.5, 0.4)
 
 dat <- tibble(x, y)
 dat
@@ -16,7 +16,7 @@ ggplot(data = dat, aes(x = x, y = y)) +
 
 ybar <- mean(y)
 xbar <- mean(x)
-beta1_hat <- sum((x - xbar) * (y - ybar)) / sum((x - xbar)^2)
+beta1_hat <- sum((x - xbar) * (y - ybar)) / sum((x - xbar) ^ 2)
 
 beta1_hat
 
@@ -38,8 +38,8 @@ sum(dists)
 
 sum(dists ^ 2)
 
-fit <- lm(y~x, data = dat)
-fit 
+fit <- lm(y ~ x, data = dat)
+fit
 
 fit$fitted.values
 fit$residuals
